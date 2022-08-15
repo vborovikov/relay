bd09b8fee48c14d5e69f0009e98c924208d0a232
Vladislav Borovikov <vborovikov@live.ru>
2022-08-15 10:17:45 +0500

    Bump version

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -2,9 +2,9 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Vladislav Borovikov")]
 [assembly: AssemblyProduct("Relay Toolkit")]
-[assembly: AssemblyCopyright("Copyright © 2021 Vladislav Borovikov")]
+[assembly: AssemblyCopyright("Copyright © 2022 Vladislav Borovikov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
-[assembly: AssemblyInformationalVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyInformationalVersion("2.3.0.0")]
