e23232853a889c9b9d942ddfe870ea9d271ae6ea
Vladislav Borovikov <vborovikov@live.ru>
2024-11-13 03:01:44 +0500

    added comments

--- Relay/RequestModel/Default/DefaultRequestScheduler.cs
+++ Relay/RequestModel/Default/DefaultRequestScheduler.cs
@@ -4,17 +4,28 @@
     using System.Threading;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Provides a base implementation of the <see cref="IRequestScheduler"/> interface.
+    /// </summary>
     public abstract class DefaultRequestScheduler : DefaultRequestDispatcherBase, IRequestScheduler
     {
         private readonly AsyncManualResetEvent scheduledEvent = new();
         private readonly IPersistentCommandStore commandStore;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DefaultRequestScheduler"/> class.
+        /// </summary>
+        /// <param name="commandStore">A <see cref="IPersistentCommandStore"/> instance to store commands.</param>
+        /// <param name="waitPeriod">A time span to wait before checking the store again.</param>
         protected DefaultRequestScheduler(IPersistentCommandStore commandStore, TimeSpan waitPeriod = default)
         {
             this.commandStore = commandStore;
             this.WaitPeriod = waitPeriod != default ? waitPeriod : TimeSpan.FromHours(24);
         }
 
+        /// <summary>
+        /// Gets the time span to wait before checking the store again.
+        /// </summary>
         public TimeSpan WaitPeriod { get; }
 
         /// <inheritdoc/>
@@ -26,6 +37,11 @@
             this.scheduledEvent.Set();
         }
 
+        /// <summary>
+        /// Processes scheduled commands execution.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
+        /// <returns>A task that represents the processing of the schedule.</returns>
         public virtual async Task ProcessAsync(CancellationToken cancellationToken)
         {
             while (true)
@@ -104,17 +120,59 @@
         }
     }
 
+    /// <summary>
+    /// Represents a persistent command.
+    /// </summary>
     public interface IPersistentCommand
     {
+        /// <summary>
+        /// Gets the command.
+        /// </summary>
         ICommand Command { get; }
+
+        /// <summary>
+        /// Gets the due time of the command execution.
+        /// </summary>
         DateTimeOffset DueTime { get; }
     }
 
+    /// <summary>
+    /// Represents a persistent command store.
+    /// </summary>
     public interface IPersistentCommandStore
     {
+        /// <summary>
+        /// Adds the command to the store.
+        /// </summary>
+        /// <typeparam name="TCommand">The type of the command.</typeparam>
+        /// <param name="command">The command.</param>
+        /// <param name="dueTime">The due time of the command execution.</param>
+        /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         Task AddAsync<TCommand>(TCommand command, DateTimeOffset dueTime, CancellationToken cancellationToken) where TCommand : ICommand;
+
+        /// <summary>
+        /// Gets the next pending command.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
+        /// <returns>A task that wraps the pending command.</returns>
         Task<IPersistentCommand?> GetAsync(CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Removes the command from the store.
+        /// </summary>
+        /// <param name="command">The persistent command object.</param>
+        /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         Task RemoveAsync(IPersistentCommand command, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Increments the retry count of the command or removes it from the store.
+        /// </summary>
+        /// <param name="command">The persistent command object.</param>
+        /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        /// <remarks>This method is called when the command execution fails. It never throws.</remarks>
         Task RetryAsync(IPersistentCommand command, CancellationToken cancellationToken);
     }
 }
